cf520d4990634632affbb5925cf8b87e22512039
Tuti Bueno <tuti.bueno@gmail.com>
2025-08-28 02:13:54 -0300

    Ajuste em migrations Ajuste no Seeder

--- app/Database/Seeds/UserSeeder.php
+++ app/Database/Seeds/UserSeeder.php
@@ -4,38 +4,42 @@ namespace App\Database\Seeds;
 
 use CodeIgniter\Database\Seeder;
 
-class AdminUserSeeder extends Seeder
+class UserSeeder extends Seeder
 {
     public function run()
     {
-        $data = [
-            [
-                'username'   => 'admin',
-                'name'       => 'Administrador',
-                'email'      => 'admin@vetrix.com.br',
-                'password'   => password_hash('123456', PASSWORD_DEFAULT),
-                'perfil'     => 'admin',
-                'created_at' => date('Y-m-d H:i:s'),
-                'updated_at' => date('Y-m-d H:i:s')
-            ],
-            [
-                'username'  => 'vet',
-                'name'      => 'Dr. Veterinário',
-                'email'     => 'veterinario@vetrix.com.br',
-                'senha'     => password_hash('123456', PASSWORD_DEFAULT),
-                'perfil'    => 'veterinario',
-                'created_at' => date('Y-m-d H:i:s'),
-            ],
-            [
-                'username'   => 'recepcao',
-                'name'      => 'Recepcionista',
-                'email'     => 'recepcao@vetrix.com.br',
-                'senha'     => password_hash('123456', PASSWORD_DEFAULT),
-                'perfil'    => 'recepcionista',
-                'created_at' => date('Y-m-d H:i:s'),
-            ],
-        ];
+        
+        $userModel = new \App\Models\UserModel();
 
-        $this->db->table('users')->insert($data);
+        $userModel->insert([
+            'username'   => 'admin',
+            'name'       => 'Administrador',
+            'email'      => 'admin@vetrix.com.br',
+            'password'   => password_hash('123456', PASSWORD_DEFAULT),
+            'perfil'     => 'admin',
+            'created_at' => date('Y-m-d H:i:s'),
+            'updated_at' => date('Y-m-d H:i:s')
+        ]);
+
+        $userModel->insert([
+            'username'  => 'vet',
+            'name'      => 'Dr. Veterinário',
+            'email'     => 'veterinario@vetrix.com.br',
+            'password'  => password_hash('123456', PASSWORD_DEFAULT),
+            'perfil'    => 'veterinario',
+            'created_at' => date('Y-m-d H:i:s'),
+        ]);
+
+        $userModel->insert([
+            'username'  => 'recepcao',
+            'name'      => 'Recepcionista',
+            'email'     => 'recepcao@vetrix.com.br',
+            'password'  => password_hash('123456', PASSWORD_DEFAULT),
+            'perfil'    => 'recepcionista',
+            'created_at' => date('Y-m-d H:i:s'),
+        ]);
+
+
+        
     }
 }
